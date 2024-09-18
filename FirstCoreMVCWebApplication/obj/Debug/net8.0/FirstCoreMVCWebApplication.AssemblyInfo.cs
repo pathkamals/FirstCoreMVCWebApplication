a165d52a8a3bd62f531f85913871709f3827c2a0
pathkamals <kamaletahe@gmail.com>
2024-09-18 14:27:50 +0530

    firstcoreapplication

--- FirstCoreMVCWebApplication/obj/Debug/net8.0/FirstCoreMVCWebApplication.AssemblyInfo.cs
+++ FirstCoreMVCWebApplication/obj/Debug/net8.0/FirstCoreMVCWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstCoreMVCWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daeab79542142f491c2b5fd16e14600079d94db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstCoreMVCWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstCoreMVCWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
